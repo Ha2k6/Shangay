58f5a3f6210bd854f90613398e37bdd40dfd996a
Mageswaran M <69040612+Ha2k6@users.noreply.github.com>
2025-11-07 08:39:25 +0530

    Implement CNN model for face recognition using LFW dataset

---
+++ 3.	Develop a CNN model for face recognition using the LFW dataset.py
@@ -0,0 +1,26 @@
+from sklearn.datasets import fetch_lfw_people
+
+import tensorflow as tf
+
+
+
+d = fetch_lfw_people(min_faces_per_person=20, resize=0.4)
+
+X = d.images[...,None]/255.0; y = d.target
+
+
+
+m = tf.keras.Sequential([tf.keras.layers.Conv2D(8,3,activation='relu',input_shape=X[0].shape),
+
+                         tf.keras.layers.Flatten(), tf.keras.layers.Dense(len(d.target_names),activation='softmax')])
+
+
+
+m.compile(optimizer='adam',loss='sparse_categorical_crossentropy')
+
+m.fit(X,y,epochs=1)
+
+print("Person:", d.target_names[tf.argmax(m.predict(X[:1]))])
+
+
+
